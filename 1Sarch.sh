3702fe6007379f0aba78ed74f0904fbd15563162
KronGeet <103604834+KronGeet@users.noreply.github.com>
2022-04-14 02:01:35 +0300

    Update 1Sarch.sh

--- 1Sarch.sh
+++ 1Sarch.sh
@@ -5,42 +5,47 @@ timedatectl set-ntp true
 sleep 2
 
 (
- echo g;
+echo g;
+echo n;
+echo 1;
+echo;
+echo +550M;
+echo;
+echo n;
+echo 2;
+echo;
+echo +2G;
+echo;
+echo n;
+echo 3;
+echo;
+echo ;
+echo t;
+echo 1;
+echo 1;
+echo t;
+echo 2;
+echo 19;
 
- echo n;
- echo 1;
- echo;
- echo +550M;
- echo;
- echo n;
- echo 2;
- echo;
- echo +2G;
- echo;
- echo n;
- echo 3;
- echo;
- echo ;
- echo t;
- echo 1;
- echo 1;
- echo t;
- echo 2;
- echo 19;
-
- echo w;
+echo w;
 ) | fdisk /dev/sda
 
 fdisk -l
-
+sleep 5
 mkfs.fat -F32 /dev/sda1
+sleep 5
 mkfs.ext4  /dev/sda3
+slwwp 5
 mount  /dev/sda3  /mnt 
-mkdir  /mnt/boot 
+sleep 5
+mkdir  /mnt/boot
+sleep 5
 mount  /dev/sda1  /mnt/boot
+sleep 5
 mkswap  /dev/sda2
+sleep 5
 swapon  /dev/sda2
+sleep 5
+pacstrap /mnt base base-devel linux linux-firmware nano
 
-pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd netctl
-
-genfstab -pU /mnt >> /mnt/etc/fstab
+genfstab -U /mnt >> /mnt/etc/fstab
